595c8a2272242e35e4add6d01e8dabbb2312afa8
Nettar-Jr <alabiabdulmumuni9@gmail.com>
2021-07-02 20:39:20 +1200

    taking and displaying the input

--- playground.php
+++ playground.php
@@ -2,15 +2,21 @@
 <?php
     if (isset($_POST['submitEmail'])){
         $email = $_POST['emial'];
+        $name = $_POST['name'];
+        $password = $_POST['password'];
 
-        echo "<h4>Welcome", $email ."</h4>";
+        echo "<h4>Welcome ", $email ."</h4>" . "<br>";
+        echo  $name ."</h4>". "<br>";
+        echo  $password ."</h4>";
     }
  ?>
 
 <!DOCTYPE html>
 <html lang="en">
     <form action="playground.php" method="POST">
-        <input name="emial" placeholder="Enter your email..."><br>
+        <input type="emial" required name="emial" placeholder="Enter your email..."><br>
+        <input type="text"  required name="name" placeholder="Enter name..."><br>
+        <input type="password" required name="password" placeholder="Enter password..."><br>
         <button type="submit" name="submitEmail">Submit</button>
     </form>
 </html>
